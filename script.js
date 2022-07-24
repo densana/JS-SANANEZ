a29ca7310707d4eb1d0602c11a9ace6dafacea8f
densana <denisesananez@gmail.com>
2022-07-24 18:20:47 -0300

    Segundfo Desafio

--- script.js
+++ script.js
@@ -1,57 +1,59 @@
-/*Saludo de Inicio*/
-let saludo = prompt("Hola, quien sos?")
-let nombre = "BRUNO";
-if (saludo == nombre) {
-    let pregunta = prompt("Hola Bruno! Queres jugar?");
-if ((pregunta == "SI") || (pregunta == "si")) {
-    let juego = prompt("selecciona una opcion de la A a la D")
-while (juego != "ESC") {
-    switch (juego) {
-        case "A":
-            let acumulador = 0
-
-            for(let i = 0; i<5 ; i++) {
-                let tazas = parseInt(prompt("Ingrese la cantidad de tazas de cafe por día de los últimos 5 días"))
-                acumulador += tazas
-            }
-            alert("El promedio de tazas x dia es " + (acumulador / 5))
-            break;
-        case "B":
-            let acumulador2 = 0
-
-            for(let i = 0; i<5 ; i++) {
-                let agua = parseInt(prompt("Ingrese la cantidad de litros de agua que tomo por día en los últimos 5 días"))
-                acumulador2 += agua
-            }
-            alert("El promedio de litros de agua x dia es " + (acumulador2 / 5))
-            break;
-        case "C":
-            let acumulador3 = 0
-
-            for(let i = 0; i<5 ; i++) {
-                let agua = parseInt(prompt("Ingrese la cantidad de cuadras que caminó por día en los últimos 5 días"))
-                acumulador3 += agua
-            }
-            alert("El promedio de cuadras caminadas x dia es " + (acumulador3 / 5))
-            break;
-        case "D":
-            let acumulador4 = 0
-
-            for(let i = 0; i<5 ; i++) {
-                let agua = parseInt(prompt("Ingrese la cantidad de horas de Netflix que miró por día, los últimos 5 días"))
-                acumulador4 += agua
-            }
-            alert("El promedio de hs de Netflix x dia es " + (acumulador4 / 5))
-            break;
-        default:
-            alert("Error");
-            break;
+/*datos de la reserva */
+function datos() {
+    const persona1 = {
+        nombre: parseInt(prompt("Ingrese su nombre")),
+        mail: parseInt(prompt("Ingrese su mail")),
+        cel: parseInt(prompt("Ingrese su número de contacto")),
     }
-    juego = prompt("Selecciona opción de la A a la E")
 }
-} else {
-    alert("Aburrido, responde si!")
-}
-} else {
-    alert("Hola! Ingresa el nombre BRUNO si queres continuar");
+
+datos()
+
+/*reserva*/
+function reservas() {
+    let reserva = prompt("Selecciona para cuantas carpas queres reservar:")
+    while (reserva != "ESC") {
+        switch (reserva) {
+            case "1":
+                    let personas = parseInt(prompt("Ingrese la cantidad de personas por carpa"))
+                alert("Reserva de una carpa para " + personas)
+                break;
+            case "2":
+                let acumulador2 = 0
+        
+                for(let i = 0; i<2 ; i++) {
+                    let personas2 = parseInt(prompt("Ingrese la cantidad de personas por carpa"))
+                    acumulador2 += personas2
+                }
+                alert("Reserva de dos carpas para " + acumulador2)
+                break;
+                case "3":
+                    let acumulador3 = 0
+        
+                    for(let i = 0; i<3 ; i++) {
+                        let personas3 = parseInt(prompt("Ingrese la cantidad de personas por carpa"))
+                        acumulador3 += personas3
+                    }
+                    alert("Reserva de tres carpas para " + acumulador3)
+                    break;
+                case "4":
+                    let acumulador4 = 0
+            
+                    for(let i = 0; i<4 ; i++) {
+                        let personas4 = parseInt(prompt("Ingrese la cantidad de personas por carpa"))
+                        acumulador4 += personas4
+                    }
+                    alert("Reserva de cuatro carpas para " + acumulador4)
+                    break;
+            default:
+                alert("Error");
+                break;
+            }
+            reserva2 = prompt("Desea hacer otra reserva?")
+        }
+    }
+
+let pregunta = prompt("Desea hacer una reserva");
+if ((pregunta == "SI") || (pregunta == "si")) {
+    reservas()
 }
