0d66f2609f14b423eab1df4dab502d0ac8d6c895
densana <denisesananez@gmail.com>
2022-08-11 20:03:49 -0300

    Desafio HTML

--- app.js
+++ app.js
@@ -11,13 +11,16 @@ const formulario = document.getElementById('formularioReservas');
 
 const users = []
 
-formulario.addEventListener('submit', (event) => {
-    event.preventDefault();
+formulario.addEventListener('click', () => {
     const username = document.getElementById('InputName').value;
     const mail = document.getElementById('InputMail').value;
     const number = document.getElementById('InputNumber').value;
+    const checkIn = document.getElementById('InputIndia').value;
+    const checkOut = document.getElementById('InputInout').value;
+    const nroPersonas = document.getElementById('InputPersonas').value;
     
     console.log(username, mail, number)
+    console.log(checkIn, checkOut, nroPersonas)
 
     const user = new User(username, mail, number)
 
@@ -28,7 +31,7 @@ formulario.addEventListener('submit', (event) => {
 });
 
 
-/*servicios*/
+/*menues*/
 
 class Menu {
     constructor(img, id, nombre, caracteristicas, precio) {
@@ -40,46 +43,63 @@ class Menu {
     }
 }
 
-const menu1 = new Menu ("aca va img","1", "Menu Desayuno", "Cafe, Te o Mate con Tostadas y galletitas varias", "$500")
-const menu2 = new Menu ("aca va img","2", "Menu Hamburguesa", "Hamburguesa carne o veggie con papas y bebida a eleccion", "$1500")
-const menu3 = new Menu ("aca va img","3", "Menu Pizza", "2 porciones pizza a eleccion + bebida a elección", "$1000")
-const menu4 = new Menu ("aca va img","4", "Menu Sandwich", "de choripan o veggie + bebida a elección", "$500")
+const menu1 = new Menu ("/images/comer.png","1", "Menu Desayuno", "Cafe, Te o Mate con Tostadas y galletitas varias", "$500")
+const menu2 = new Menu ("/images/hamburguesa-con-queso.png","2", "Menu Hamburguesa", "Hamburguesa carne o veggie con papas y bebida a eleccion", "$1500")
+const menu3 = new Menu ("/images/comer.png","3", "Menu Pizza", "2 porciones pizza a eleccion + bebida a elección", "$1000")
+const menu4 = new Menu ("/images/comer.png","4", "Menu Sandwich", "de choripan o veggie + bebida a elección", "$500")
 
 
 const menues = [menu1, menu2, menu3, menu4]
 
+
+/*servicios + carrito*/
 const servicios = document.querySelector('#contenedorServicios')
+
+const tBodyCarrito = document.querySelector('#tBodyCarrito')
+
+const carrito = []
 
 menues.forEach(arrayServicios => {
     servicios.innerHTML += `
     <div class="col-sm-3">
             <div class="card style="width: 18rem;">
               <div class="card-body">
-                <img src="${arrayServicios.img}" alt="icon">
+                <img class="img-menu" src="${arrayServicios.img}" alt="icon">
                 <h5 class="card-title">${arrayServicios.nombre}</h5>
                 <p class="card-text">${arrayServicios.caracteristicas}</p>
-                <button id="add" class="btn btn-primary">Agregar a carrito</button>
+                <button type="button" class="btn btn-primary">Agregar menu ${arrayServicios.id}</button>
               </div>
             </div>
           </div>
     `
-
-    const boton = document.getElementById("add")
-
-    boton.addEventListener('click', () => {
-        /*agregarAlCarrito(arrayServicios.id)*/
-        console.log("cucu")
+    const botonesComprar = document.querySelectorAll('.btn')
+    botonesComprar.forEach((botonDeCompra) => {
+    botonDeCompra.addEventListener('click', () => {
+        agregarAlCarrito()
     })
-
+})
 });
 
 /*carrito*/
 
-const carrito = []
-
 const agregarAlCarrito = (menuId) => {
-    const item = menues.find((prod) => prod.id === menuId)
+    const item = menues.find((servicio) => servicio.id === menuId)
     carrito.push(item);
+    estadoCarrito();
     console.log(carrito);
+}
+
+const estadoCarrito = () => {
+
+    carrito.forEach(servicio => {
+       tBodyCarrito.innerHTML += `
+       <tr>
+       <th scope="row">${servicio.id}</th>
+       <td>${servicio.nombre}</td>
+       <td>${servicio.caracteristicas}</td>
+       <td>${servicio.precio}</td>
+       </tr>
+       `
+  })
 }
 
